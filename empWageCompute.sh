fe3c8ed832524ed887a4c23f1498a49211249709
mrun255dumbre <mrun255dumbre@gmail.com>
2020-08-09 15:04:20 +0530

    Employee absent or present

--- empWageCompute.sh
+++ empWageCompute.sh
@@ -2,3 +2,14 @@
 
 echo "welcome to Emp Wage Computation"
 
+function getWorkHrs ()
+{
+        Random=((RANDOM%2))
+        if [ $Random -eq 1 ]
+        then
+                echo "Employee is Present"
+        else
+                echo "Employee is Absent"
+        fi
+}
+
